3942a343b5ad823492dd82df0022a07683c26536
Bruce S. Perez <minibrusp@gmail.com>
2021-04-02 01:23:43 +0800

    Responsive Design

--- script/script.js
+++ script/script.js
@@ -4,7 +4,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
    const shareButton = document.querySelector('.button__share');
    shareButton.addEventListener('click', (event) => {
       let socialIcons = document.querySelector('aside');
+      socialIcons.classList.add('aside__show__transition')
       socialIcons.classList.toggle('aside__show');
       shareButton.classList.toggle('button__share__clicked');
    });
+   
 });
